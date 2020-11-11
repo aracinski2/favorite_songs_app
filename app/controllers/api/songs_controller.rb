5e51f6cbd5a149bfcf29e7874c3ea9f508ab6cd5
Alex Racinski <aracinski2@gmail.com>
2020-11-10 19:25:32 -0600

    add song create and update methods

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -10,4 +10,25 @@ class Api::SongsController < ApplicationController
     render 'show.json.jb'
   end
 
+  def create
+    @song = Song.new(
+      title: params[:title],
+      album: params[:album],
+      artist: params[:artist],
+      year: params[:year]
+    )
+    @song.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:title]
+    @song.album = params[:album]
+    @song.artist = params[:artist]
+    @song.year = params[:year]
+    @song.save
+    render 'show.json.jb'
+  end
+
 end
